3414f43f3a7784af8c2ac20264df4c4fccf7ee25
becem-dev <you@example.com>
2020-10-17 08:48:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Particles from "react-particles-js";
+const PARTICLES_PARAMS = {
+  particles: {
+    number: {
+      value: 100,
+    },
+    size: {
+      value: 6,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hey
+      <Particles params={PARTICLES_PARAMS} />
     </div>
   );
 }
